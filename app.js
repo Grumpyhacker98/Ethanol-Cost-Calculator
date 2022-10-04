650c1f04c4062fe06740742259443413a8dae7c2
Maximilian Lewis <max.a.lewis98@gmail.com>
2022-10-03 23:06:26 -0700

    added inquirer prompts

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// import inquirer from 'inquirer';
-const cheerio = require('cheerio');
-const axios = require('axios');
+import inquirer from 'inquirer';
+import cheerio from 'cheerio';
+import axios from 'axios';
 
 // https://distillique.co.za/blogs/default-blog/is-it-worthwhile-to-distill-bio-ethanol-as-fuel
 // https://npm.io/package/cheerio
@@ -10,10 +10,10 @@ const axios = require('axios');
 console.log("app start");
 
 // scrape current global food costs
-axios.get("https://markets.businessinsider.com/commoditie")
+axios.get("https://markets.businessinsider.com/commodities")
     .then((response) => {
-        console.log("business insider scrape successful");
 
+        console.log("business insider scrape successful");
         var $ = cheerio.load(response.data);
 
         var agricultureData = $(".table__th:contains('Agriculture')").parents().parents().parents();
@@ -37,26 +37,55 @@ axios.get("https://markets.businessinsider.com/commoditie")
         })
 
         console.log(scrappedData)
+
+        // mash/wash that reaches higher ABV is more efficient
+        // calculate all sugar wash, grain mash, corn mash and their cost/effectiveness, chose cheapest OR prompt to chose 
+
+
+        // grab energy bill info + distillery size + labor costs + capital investment
+        inquirer.prompt([{
+            type: 'number',
+            name: 'stillSize',
+            message: 'How large is the size of your distillery? (Gallons)'
+        }, {
+            type: 'number',
+            name: 'electricCost',
+            message: 'What is the current rate of your electric bill? (KW/hr)'
+            // if defaulted scrape for global average cost?
+        }, {
+            type: 'number',
+            name: 'laborCost',
+            message: 'What price do you value your free time? (USD/Hour)'
+        }, {
+            type: 'number',
+            name: 'capitalInvestment',
+            message: 'How much capital for all the equipment? (USD)'
+        }]).then((answers) => {
+
+            console.log(answers)
+
+            // calculate energy required to boil
+            // total yield/cost for a single run
+
+
+            // find gals require to return capital investment using ...
+            // 100% Ethanol
+            // 85% E85
+            // 15% E15
+            // add cost of labor variable
+
+
+
+        }).catch((error) => {
+            console.log(error);
+            console.log("Sorry, inqurirer ran into an error")
+        });
     })
     .catch((error) => {
-        // handle error
         console.log(error);
-        console.log("Buisness Insider scrape failed, sorry. error code above");
+        console.log("Buisness Insider web scrape failed, sorry");
     })
-    // inquirer.prompt([{
-
-    // }])
 
 
-// calculate all sugar wash, grain mash, corn mash and their cost/effectiveness, chose cheapest
 
-// grab energy bill/kwh from user
-// might need still size + other
-// calculate energy required to boil
-// total yield/cost for a single run
 
-// find gals require to return capital investment using ...
-// 100% Ethanol
-// 85% E85
-// 15% E15
-// add cost of labor variable
